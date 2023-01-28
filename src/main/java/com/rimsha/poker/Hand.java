f9d0cdee5b9fd444796c4dcd3aab85823a63fe27
notenger <116970623+notenger@users.noreply.github.com>
2023-01-28 20:47:20 +0300

    Created Hand Class
    Class for holding a set of cards

---
+++ src/main/java/com/rimsha/poker/Hand.java
@@ -0,0 +1,24 @@
+package com.rimsha.poker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+
+    private List<Card> cards = new ArrayList<>();
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    @Override
+    public String toString() {
+        return "Hand{" +
+                "cards=" + cards +
+                '}';
+    }
+}
