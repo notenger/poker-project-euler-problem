73d460983dfd56b7614ad7b07a81b92db632fd22
notenger <116970623+notenger@users.noreply.github.com>
2023-01-28 22:05:01 +0300

    Added logic for counting each player wins
    Procedure for obtaining of wins distribution based on input file is written

---
+++ src/main/java/com/rimsha/poker/Poker.java
@@ -0,0 +1,30 @@
+package com.rimsha.poker;
+
+import java.util.*;
+
+public class Poker {
+
+    public static void main(String[] args) {
+
+        CombinationsLoader.loadCombinations();
+
+        int winsFirst = 0;
+        int winsSecond = 0;
+
+        for (Hand[] play : CombinationsLoader.getPlays()) {
+            Hand handOne = play[0];
+            Hand handTwo = play[1];
+
+            Game game = new Game(handOne, handTwo);
+            Players winner = game.chooseWinner();
+            if (winner == Players.FIRST) winsFirst++;
+            if (winner == Players.SECOND) winsSecond++;
+        }
+
+        System.out.println(
+                String.format("The first player won %s times\nThe second player won %s times", winsFirst, winsSecond)
+        );
+
+    }
+
+}
