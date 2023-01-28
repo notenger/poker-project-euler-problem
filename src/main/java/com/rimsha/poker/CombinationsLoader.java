df75e5a496a33cdd08df28db21bfad788a8bf4c4
notenger <116970623+notenger@users.noreply.github.com>
2023-01-28 21:52:28 +0300

    Created CombinationsLoader Class
    Class for reading input data and initializing plays

---
+++ src/main/java/com/rimsha/poker/CombinationsLoader.java
@@ -0,0 +1,46 @@
+package com.rimsha.poker;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationsLoader {
+
+    private static List<Hand[]> plays = new ArrayList<>();
+
+    public static void loadCombinations() {
+        BufferedReader reader;
+
+        try {
+            reader = new BufferedReader(new FileReader("src/main/resources/p054_poker.txt"));
+            String line = reader.readLine();
+
+            while (line != null) {
+                String[] cardsLine = line.split(" ");
+
+                Hand[] hands = {new Hand(), new Hand()};
+
+                for (int i = 0; i < 10; i++) {
+                    Card card = Card.createCard(cardsLine[i]);
+                    if (i < 5) hands[0].addCard(card);
+                    else hands[1].addCard(card);
+                }
+
+                plays.add(hands);
+
+                line = reader.readLine();
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static List<Hand[]> getPlays() {
+        return plays;
+    }
+}
