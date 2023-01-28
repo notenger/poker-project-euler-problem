4e64397289bf133ce36e7a49251768c17070d894
notenger <116970623+notenger@users.noreply.github.com>
2023-01-28 20:44:27 +0300

    Created Card Class
    Class representing a playing card of a certain rank and suit

---
+++ src/main/java/com/rimsha/poker/Card.java
@@ -0,0 +1,46 @@
+package com.rimsha.poker;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.SimpleTimeZone;
+
+public class Card implements Comparable<Card> {
+    private final Rank rank;
+    private final Suit suit;
+
+    public Card(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+
+    @Override
+    public int compareTo(Card o) {
+        return rank.getOrder() - o.getRank().getOrder();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return rank == card.rank && suit == card.suit;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(suit);
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
